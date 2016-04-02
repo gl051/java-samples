450e8ff6153ae144fd9ae18231c39e6454eb3829
gl051 <gl051@biccari.com>
2016-04-01 17:47:58 -0700

    Improved sample selector method

--- src/com/gl051/JavaSamples.java
+++ src/com/gl051/JavaSamples.java
@@ -5,13 +5,32 @@ public class JavaSamples {
     public static void main(String[] args) {
         System.out.println("*** Tour De Java ***");
         ISample s = getSample();
-        System.out.println("Sample description: " + s.getDescription());
-        s.run();
+        if (s != null)
+        {
+        	System.out.println("Sample description: " + s.getDescription());
+        	s.run();
+        }
+        else
+        {        	
+        	System.out.println("Can't find the sample to run");
+        }
     }
     
     // Get the Sample to execute 
     private static ISample getSample(){
-        return new com.gl051.string.Sample();
+    	// Manually choose what you want to run
+    	String choice = "OOP";
+    	
+    	switch (choice)
+    	{
+    		case "OOP":
+    			return new com.gl051.oop.Sample();
+    		case "STRING":
+    			return new com.gl051.string.Sample();
+    		default:
+    			return null;
+    	}
+        
     }
 
 }
